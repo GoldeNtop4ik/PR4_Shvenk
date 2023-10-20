d8399a45eda9a94109403a87c3d0e90a0189136c
Stepan <shvenk.stepan@mail.ru>
2023-10-20 17:17:44 +0700

    pr4

---
+++ client/client.py
@@ -0,0 +1,25 @@
+﻿import socket
+from _thread import *
+
+client = socket.socket()
+hostname = socket.gethostname()
+port = 12345
+client.connect((hostname,port))
+
+name = input("Имя: ")
+client.send(name.encode())
+
+def Send():
+    while True:
+        message = input("Введите текст")
+        client.send(message.encode())
+def Take():
+    while True:
+        data = client.recv(1024)
+        print(data.decode())
+        
+start_new_thread(Send, ())
+start_new_thread(Take, ())
+
+while True:
+    a = 1
---
+++ server/server.py
@@ -0,0 +1,28 @@
+﻿import socket
+from _thread import *
+
+def client_thread(con):
+    data = con.recv(1024)
+    name = data.decode()
+    while True:
+        data = con.recv(1024)
+        message = f"\n{name}: {data.decode()}"
+        print(f"{message}")
+        for i in clients:
+            if i != con:
+                i.send(message.encode())
+
+server = socket.socket()
+hostname = socket.gethostname()
+port = 12345
+server.bind((hostname, port))
+server.listen(5)
+
+clients = []
+
+print("Сервер запущен")
+
+while True:
+    client, _ = server.accept()
+    clients.append(client)
+    start_new_thread(client_thread, (client,))
